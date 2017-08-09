e3e0a096aa9cea134a4b4784e3c2008d729aaace
salimbasar <salimbasar@gmail.com>
2017-08-09 13:26:17 +0300

    English translations added
    English translations for the non-turkish users.

--- samples/purchase.php
+++ samples/purchase.php
@@ -24,10 +24,11 @@ $gateway->setSecretKey('QOClasdJUuDDWasdasdasd');
 $gateway->setBank('ISBANK');
 
 $gateway->setMode("NonDirectPost3D");
-//Diğer paremetreler: api DirectPost3D NonDirectPost3D
-//3D test için işlem şifresi a ya da 1
+//Diğer paremetreler - Other Parameters: api DirectPost3D NonDirectPost3D
 
-// Zorunlu parametreler
+//3D test için işlem şifresi: a ya da 1 - For 3D test secure password "a" or "1"
+
+// Zorunlu parametreler - Necessary Parameters
 $card = [
     'number'        => '5456165456165454',
     'expiryMonth'   => '12',
@@ -55,25 +56,25 @@ try {
     $options = [
         'amount'        => 100.00,
         'currency'      => 'TRY',
-        //'installment'   => 0, // Taksit
-        'orderId'       => 'S-12341308', // Benzersiz olmalı.
+        //'installment'   => 0, // Taksit - Installment
+        'orderId'       => 'S-12341308', // Benzersiz olmalı - Must be unique
         'returnUrl'     => 'http://local.desktop/Paratika/callback.php',
         'cancelUrl'     => 'http://local.desktop/Paratika/callback.php',
-        'sessionType'   => 'PAYMENTSESSION', //Diğer parametreler: PAYMENTSESSION WALLETSESSION
+        'sessionType'   => 'PAYMENTSESSION', //Diğer parametreler - Other Parameters: PAYMENTSESSION WALLETSESSION
         'card'          => $card,
     ];
 
-    // SessionToken almak için oturum açalım
+    // SessionToken almak için oturum açalım - Let's open a session to get a SessionToken
     $sessionResponse = $gateway->session($options)->send();
     
     if ($sessionResponse->isSuccessful()) {
         
             $sessionToken =  $sessionResponse->getSessionToken();
 
-            // Oturum değiştikenini satış ve diğer işlemlerde kullanmak için tanımlayalım.
+            // Oturum değiştikenini satış ve diğer işlemlerde kullanmak için tanımlayalım - Let's define session variable to use in Auth (sell) and other actions
             $gateway->setSessionToken($sessionToken);
 
-            // Auth (Satış) işlemi
+            // Auth (Satış) işlemi - Auth (Sell) action
             $response = $gateway->purchase($options)->send();
 
             if ($response->isSuccessful()) {
